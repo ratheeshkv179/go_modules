04fa8d1080776af9ffed896663c78d3d494c018f
ratheeshkv179 <ratheeshkv179@gmail.com>
2020-03-20 10:36:56 +0530

    Difference code updated

--- numbers/numbers.go
+++ numbers/numbers.go
@@ -1,9 +1,19 @@
 package numbers
 
+import (
+	"errors"
+)
+
+// Sum of input values
 func Sum(a int, b int) int {
 	return a + b
 }
 
-func Difference(a int, b int) int {
-	return a - b
+// Difference of input values
+func Difference(a int, b int) (int, error) {
+	if a >= b {
+		return a - b, nil
+	}
+	return 0, errors.New("Invalid Input")
+
 }
